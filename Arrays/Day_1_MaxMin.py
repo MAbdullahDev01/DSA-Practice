0249da9f2821aa8a34bf6a5a4a8e5baf4c9353fe
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-01-12 21:40:27 +0500

    feat(Arrays): solved max/min array problem using iteration and built-in functions

---
+++ Arrays/Day_1_MaxMin.py
@@ -0,0 +1,27 @@
+# Problem: Find the maximum and minimum in an array.
+# Learning Objectives: Array traversal, comparison, Python max, min.
+
+from utils.Random_List_Generator import Random_List_Generator
+
+# Generate a random list
+num_list = Random_List_Generator(15, 1, 100)
+print("Random List:", num_list)
+
+# Manual max/min calculation
+# Time complexity for manual solution is: O(n)
+max_value = num_list[0]
+min_value = num_list[0]
+
+for num in num_list:
+    if num > max_value:
+        max_value = num
+    if num < min_value:
+        min_value = num
+
+print(f"Maximum value is: {max_value}")
+print(f"Minimum value is: {min_value}")
+
+# Optional: Using built-in functions
+print("Using built-in functions:")
+print("Max:", max(num_list))
+print("Min:", min(num_list))
